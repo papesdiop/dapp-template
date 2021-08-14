e91b3113371fd89e897160df25a89724d84c00a0
Pape Samba <papesdiop@gmail.com>
2021-08-14 18:04:55 +0000

    refactoring ui with formik

--- src/pages/Multisig/Actions/index.tsx
+++ src/pages/Multisig/Actions/index.tsx
@@ -1,3 +1,8 @@
+
+import { Formik } from 'formik';
+import { Box, Button } from 'rebass';
+import { Label, Input } from '@rebass/forms';
+
 import * as React from "react";
 import * as Dapp from "@elrondnetwork/dapp";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
@@ -6,7 +11,7 @@ import { contractAddress, } from "config";
 import { routeNames } from "routes";
 import {} from "module";
 import {Address, Balance, BigUIntValue, BytesValue, ContractFunction, GasLimit, SmartContract, TypedValue, U32Value} from "@elrondnetwork/erdjs";
-
+//import ProductFormAdvanced from '../../../components/Formiker/ProductFormAdvanced';
 
 const Actions = () => {
   const sendTransaction = Dapp.useSendTransaction();
@@ -26,54 +31,151 @@ const Actions = () => {
       transaction,
       callbackRoute: routeNames.transaction,
     });
+
   };
 
-  const proposeSendEgld = (address: string, amount: number) => (e: React.MouseEvent) => {
+  /**
+   * propose to send EGLD to a receiver address
+   * @param values contains the receiver address and amount
+   */
+  const proposeSendEgld = (values: any)  => (e: React.MouseEvent)  => {
     e.preventDefault();
-    console.log(' --- track data : ', address, amount);
-    const args = [BytesValue.fromHex(new Address(address).hex()), new BigUIntValue(Balance.egld(amount).valueOf())];
+    console.log(' --- track data : ', values.address, values.amount);
+    const args = [BytesValue.fromHex(new Address(values.address).hex()), new BigUIntValue(Balance.egld(values.amount).valueOf())];
     proposeAction("proposeSendEgld", args);
   }
 
-  const deposit = (value:number) => (e: React.MouseEvent) => {
-    e.preventDefault();
-    proposeAction("deposit",[],Balance.egld(value));
+  /**
+   * 
+   * @param values 
+   * @returns 
+   */
+  const deposit = (values: any) => (e: React.MouseEvent)  => {
+    const {amount} = values;
+    proposeAction("deposit",[],Balance.egld(amount));
   }
 
-  // eslint-disable-next-line
-  const signActionById = (actionId: number) => (e: React.MouseEvent) => {
-    e.preventDefault();
-    const args = [ new U32Value(actionId)];
-    proposeAction("sign", args);
+  // // eslint-disable-next-line
+  // const signActionById = (actionId: number) => (e: React.MouseEvent) => {
+  //   e.preventDefault();
+  //   const args = [ new U32Value(actionId)];
+  //   proposeAction("sign", args);
+  // }
+
+  // // eslint-disable-next-line
+  // const unSignActionById = (actionId: number) => (e: React.MouseEvent) => {
+  //   e.preventDefault();
+  //   const args = [ new U32Value(actionId)];
+  //   proposeAction("unsign", args);
+  // }
+
+  // // eslint-disable-next-line
+  // const performActionById = (actionId: number) => (e: React.MouseEvent) => {
+  //   e.preventDefault();
+  //   const args = [ new U32Value(actionId)];
+  //   proposeAction("performAction", args);
+  // }
+
+  // const [address, setAddres]  = React.useState(""); //'erd17*upr269rd980g0pu7f7xhdmswyr7hxhds8yt8dm7pg903pyy69strr5u*';
+  // const [amount, setAmount] = React.useState("");;
+  // const [actionId, setActionId] = React.useState("");;
+
+
+  // const handleChangeAddress = (e: { target: { value: React.SetStateAction<string>; }; }) => setAddres(e.target.value);
+  // const handleChangeAmount = (e: { target: { value: React.SetStateAction<string>; }; }) => setAmount(e.target.value);
+  // const handleChangeActionId = (e: { target: { value: React.SetStateAction<string>; }; }) => setActionId(e.target.value);
+
+  const validateForm = (values: { address: string; amount: string; }) => {
+    const errors = {address:'', amount:''};
+    if (!values.address) {
+      errors.address = 'Required';
+    }
+    if (!values.amount) {
+      errors.amount = 'Required';
+    }
+    return errors;
   }
-
-  // eslint-disable-next-line
-  const unSignActionById = (actionId: number) => (e: React.MouseEvent) => {
-    e.preventDefault();
-    const args = [ new U32Value(actionId)];
-    proposeAction("unsign", args);
-  }
-
-  // eslint-disable-next-line
-  const performActionById = (actionId: number) => (e: React.MouseEvent) => {
-    e.preventDefault();
-    const args = [ new U32Value(actionId)];
-    proposeAction("performAction", args);
-  }
-
-  const [address, setAddres]  = React.useState(""); //'erd17*upr269rd980g0pu7f7xhdmswyr7hxhds8yt8dm7pg903pyy69strr5u*';
-  const [amount, setAmount] = React.useState("");;
-  const [actionId, setActionId] = React.useState("");;
-
-
-  const handleChangeAddress = (e: { target: { value: React.SetStateAction<string>; }; }) => setAddres(e.target.value);
-  const handleChangeAmount = (e: { target: { value: React.SetStateAction<string>; }; }) => setAmount(e.target.value);
-  const handleChangeActionId = (e: { target: { value: React.SetStateAction<string>; }; }) => setActionId(e.target.value);
-
 
   return (
-    <div className="d-flex mt-4 justify-content-center">
+     <Box
+      sx={{
+        maxWidth: 512,
+        mx: 'auto',
+        px: 3,
+      }}>
+     <h1>Multisig Account Management</h1>
+      <Formik
+      initialValues={{
+        address: '',
+        amount: '',
+      }}
+      validate={validateForm}
+      onSubmit={()=>{}}
+      >
+          {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <Box>
+                <Label htmlFor='address'>Address</Label>
+                <Input
+                    type="text"
+                    name="address"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.address}
+                    id='address'/>
+                {errors.address && touched.address && errors.address}
+              </Box>
+              <Box>
+                <Label htmlFor='amount'>Amount</Label>
+                <Input
+                    type="text"
+                    name="amount"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.amount}
+                    id='amount'/>
+                {errors.amount && touched.amount && errors.amount}
+              </Box>
+              
+              <a
+                href={routeNames.home}
+                onClick={proposeSendEgld(values)}
+                className="text-white text-decoration-none"
+              >
+                Propose
+              </a>
+
+              <a
+                href={routeNames.home}
+                onClick={deposit(values)}
+                className="text-white text-decoration-none"
+              >
+                Deposit
+              </a>
+
+              <li>{values.address}</li>
+            </form>
+          )}
+    </Formik>
+    </Box>
+  );
+};
+
+export default Actions;
+
+/*
+<div className="d-flex mt-4 justify-content-center">
       <div className="action-btn">
+      <ProductFormAdvanced />
         Address <input type="text" placeholder="Put the address here..."
           value={address}
           onChange={handleChangeAddress} />
@@ -122,7 +224,4 @@ const Actions = () => {
         </a>
       </div>
     </div>
-  );
-};
-
-export default Actions;
+*/
